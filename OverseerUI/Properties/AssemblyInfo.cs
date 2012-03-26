17abf4c621744c1ec474df9cf578cf37f3ca4620
jaredtait <jared.tait@gmail.com>
2012-03-26 14:49:02 -0700

    update screenwatchui properties

--- OverseerUI/Properties/AssemblyInfo.cs
+++ OverseerUI/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ParentsEyes")]
+[assembly: AssemblyProduct("ScreenWatchUI")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
